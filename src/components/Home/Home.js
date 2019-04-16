f2f123a8aabee38788fb58238e81eb705e8b37e9
Jacob Ankney <Jacob@Jacobs-MacBook-Pro.local>
2019-04-16 15:03:42 -0600

    second commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom'
 export default class Home extends Component {
   render() {
     return (
       <div className="box">
-        <button className='btn'>Math 1010</button>
+
+        <Link to="/classlist/math1010">
+          <button className='btn'>Math 1010</button>
+        </Link>
+       
+        <Link to="/classlist/english2010">
         <button className='btn'>English 2010</button>
+        </Link>
+
+        <Link to="/classlist/biology2020">
         <button className='btn'>Biology 2020</button>
+        </Link>
       </div>
     );
   }
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
+import  axios from 'axios';
+
 
 export default class Student extends Component {
   constructor() {
     super()
-
+    this.state= {
+      studentInfo: {}
+    }
   }
+
+componentDidMount() {
+  axios.get(`http://localhost:3005/students/${this.props.match.params.id}`).then(
+    res=> this.setState({studentInfo: res.data})
+  ).catch((err)=console.log(err, "error:operator"))
+}
 
   render() {
     return (
       <div className="box">
         <h1>Student</h1>
-      </div>
+        <h3>
+          {this.state.grade}
+        </h3>
+        <h3>
+        {this.state.email}
+        </h3> 
+        
+        </div>
     )
   }
 }
